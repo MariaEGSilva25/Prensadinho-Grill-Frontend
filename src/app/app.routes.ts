41884759a50738e6309253f4462f7af3249935ac
MariaEGSilva25 <mariaeduardagomesdasilva2004@gmail.com>
2025-04-30 16:48:53 -0300

    feat: demais arquivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { VinhetaComponent } from './pages/components/vinheta/vinheta.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component:VinhetaComponent },
+  { path: 'login', component:LoginComponent },
+
+];
