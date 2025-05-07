605496413377c9fd933757b469907566d82e8e16
MariaEGSilva25 <mariaeduardagomesdasilva2004@gmail.com>
2025-05-07 02:14:32 -0300

    feat: finalizando estilo do menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../components/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  rotinasIcon = [
+    { nome: 'Cadastro', icon: '/assets/Cadastro.png' },
+    { nome: 'Venda', icon: '/assets/Venda.png' },
+    { nome: 'Estoque', icon: '/assets/Estoque.png' },
+    { nome: 'Fiado', icon: '/assets/Fiado.png' },
+  ];
 }
