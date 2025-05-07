836e0e50e28d10b0e9096142edf7520dd949dd24
MariaEGSilva25 <mariaeduardagomesdasilva2004@gmail.com>
2025-05-07 12:06:40 -0300

    feat: criando componentes para navegação da aplicação

---
+++ src/app/pages/fiado/fiado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fiado',
+  standalone: true,
+  imports: [],
+  templateUrl: './fiado.component.html',
+  styleUrl: './fiado.component.css'
+})
+export class FiadoComponent {
+
+}
---
+++ src/app/pages/venda/venda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-venda',
+  standalone: true,
+  imports: [],
+  templateUrl: './venda.component.html',
+  styleUrl: './venda.component.css'
+})
+export class VendaComponent {
+
+}
