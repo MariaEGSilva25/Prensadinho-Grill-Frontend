dbee81e39eefd71f6586189dd0f185413589191c
MariaEGSilva25 <mariaeduardagomesdasilva2004@gmail.com>
2025-05-07 01:24:01 -0300

    feat: implementando login  e roteamento para a pagfina home, iniciando a pagina home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { VinhetaComponent } from './pages/components/vinheta/vinheta.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component:VinhetaComponent },
   { path: 'login', component:LoginComponent },
+  { path: 'home', component:HomeComponent },
 
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,42 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
 
-  constructor() {
+  constructor(private route: Router) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     })
   }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      console.log('Login enviado:', loginData);
+
+      this.route.navigate(['/home'], { queryParams: { email: loginData.email } });
+
+    } else {
+      console.log('Formulário inválido');
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+
+  ngOninit() {
+    console.log("estou sendo executado", this.loginForm);
+  }
+
 }
