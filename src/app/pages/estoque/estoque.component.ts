7295823990c3392821c2efdf20c96ba80212b972
MariaEGSilva25 <mariaeduardagomesdasilva2004@gmail.com>
2025-05-15 20:24:09 -0300

    feat: layout finalizado

--- src/app/pages/estoque/estoque.component.ts
+++ src/app/pages/estoque/estoque.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-estoque',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './estoque.component.html',
   styleUrl: './estoque.component.css'
 })
 export class EstoqueComponent {
-
+  produtos = [
+    { id: '000001', descricao: 'Coca Cola 2L', estoque: 30, preco: 12.00 },
+    { id: '000002', descricao: 'Guaraviton', estoque: 55, preco: 4.00 }
+  ];
 }
--- src/app/pages/fiado/fiado.component.ts
+++ src/app/pages/fiado/fiado.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-fiado',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './fiado.component.html',
   styleUrl: './fiado.component.css'
 })
 export class FiadoComponent {
+  clientes = [
+    { id: '000001', nome: 'Seu Chico',  notaPendente: "Nota Anexada", telefone: "11 4002-8922", valor: 12.00, data: "09/11" },
+    { id: '000002', nome: 'Benones',  notaPendente: "Nota Anexada", telefone: "11 4002-8527", valor: 10.00, data: "09/11" },
 
+  ];
 }
